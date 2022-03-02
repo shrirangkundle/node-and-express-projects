ed56b0185fcdb4aa0a963a01c573f14b7339f20f
Shrirang <shrirangkundle10@gmail.com>
2022-03-02 16:01:54 +0530

    mongo connection implemetation

--- task-manager/app.js
+++ task-manager/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 //middleware
 console.log("git changes");
@@ -14,7 +16,15 @@ app.get("/hello", (req, res) => {
 });
 
 app.use("/api/v1/tasks", tasks);
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(port, () => {
-  console.log(`server listening on port ${port}`);
-});
+start();
--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,12 +1,13 @@
+const tasks = require("../models/tasks");
 const getAllTasks = (req, res) => {
   res.send("all items");
 };
 
 const createTask = (req, res) => {
-  res.send("Create tesk");
+  res.json(req.body);
 };
 const getTask = (req, res) => {
-  res.send("get single tesk");
+  res.json({ id: req.params.id });
 };
 const updateTask = (req, res) => {
   res.send("update tesk");

1e34293d4dca35ccc89e4c209c2572360a07fd41
Shrirang <shrirangkundle10@gmail.com>
2022-03-06 20:07:14 +0530

    DS filter query params

--- demo-store-api/controllers/products.js
+++ demo-store-api/controllers/products.js
@@ -1,10 +1,23 @@
+//model
+const Product = require("../models/product");
+
+//testing api
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async error");
-  res.status(200).json({ msg: "products testing routes static" });
+  const products = await Product.find({
+    company: "marcos",
+  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products testing routes" });
+  const { featured } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {

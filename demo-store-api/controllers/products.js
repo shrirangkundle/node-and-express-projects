c349c87debe3e58ef0665810679f983fe3f83ce0
Shrirang <shrirangkundle10@gmail.com>
2022-03-06 20:38:24 +0530

    DS regex implementation

--- demo-store-api/controllers/products.js
+++ demo-store-api/controllers/products.js
@@ -3,18 +3,25 @@ const Product = require("../models/product");
 
 //testing api
 const getAllProductsStatic = async (req, res) => {
+  const search = "aaa";
   const products = await Product.find({
-    company: "marcos",
+    name: { $regex: search, $options: "i" },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
   const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });

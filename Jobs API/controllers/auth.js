78074466e237c3a6fda499ff33a47371baf0091a
Shrirang <shrirangkundle10@gmail.com>
2022-03-15 16:26:02 +0530

    JWT implementation

--- Jobs API/controllers/auth.js
+++ Jobs API/controllers/auth.js
@@ -1,9 +1,36 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
+
 const register = async (req, res) => {
-  res.send("registered user");
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send("login user");
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const user = await User.findOne({ email });
+
+  //compare password
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Cred");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Cred");
+  }
+
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {

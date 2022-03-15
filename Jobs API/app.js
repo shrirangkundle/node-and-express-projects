e29418dbe97126b1bfa79dca5682f29b3a7e0000
Shrirang <shrirangkundle10@gmail.com>
2022-03-15 23:11:55 +0530

    security update

--- Jobs API/app.js
+++ Jobs API/app.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+//extra security packages
+
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
 const express = require("express");
 const app = express();
 
@@ -15,8 +21,16 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
+
+//first param is num of minutes which is 15
+//second param is no of req per ip : 100
+
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
--- Jobs API/middleware/error-handler.js
+++ Jobs API/middleware/error-handler.js
@@ -1,10 +1,33 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    // set defaults
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "something went wrong, try again later",
+  };
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
 
-module.exports = errorHandlerMiddleware
+    customError.statusCode = 400;
+  }
+  if (err.name === "CastError") {
+    customError.msg = "invalis id , no item found";
+    customError.statusCode = 404;
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
